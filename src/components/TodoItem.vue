1909801fb7e5f6ee12b9a23149fd058fd5044deb
Riin Fruit <nassii74@gmail.com>
2020-03-22 23:01:25 +0900

    feat: TodoItem.vue

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,34 +1,35 @@
 <template>
   <li>
     <span>{{ item }}</span>
+    <span>{{ item.message }}</span>
     <span>{{ message }}</span>
-    <span>{{ count }}</span>/
-    <span>{{ double }}</span>/
-    <span>{{ $sample.triple(double)}}</span>
+    <span>{{ count }}</span> / <span>{{ double }}</span> /
+    <span>{{ $sample.triple(double) }}</span>
   </li>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue, { PropType } from "vue";
+export type itemType = {
+  message: string;
+};
+
+export default Vue.extend({
   props: {
-    item: {
-      Type: Object
-    },
-    count: {
-      type: Number
-    }
+    item: Object as PropType<itemType>,
+    count: Number
   },
   data() {
     return {
-      message: "sample",
+      message: "sample"
     };
   },
   computed: {
-    double() {
+    double(): number {
       return this.count * 2;
     }
   }
-};
+});
 </script>
 
 <style scoped></style>
